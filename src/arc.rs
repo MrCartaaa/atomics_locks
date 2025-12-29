62aa1c38581ff4f8b1e2cfd158e5439579c76ce1
Carter Steele <carter@maetech.ca>
2025-12-29 17:09:28 -0500

    adding full functionality for the Arc and creating a test with a SpinLock

--- src/arc.rs
+++ src/arc.rs
@@ -1,3 +1,5 @@
+use std::cell::UnsafeCell;
+use std::mem::ManuallyDrop;
 use std::ops::Deref;
 use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
 use std::sync::atomic::fence;
@@ -5,7 +7,8 @@ use std::{ptr::NonNull, sync::atomic::AtomicUsize};
 
 struct ArcData<T> {
     ref_count: AtomicUsize,
-    data: T,
+    weak_count: AtomicUsize,
+    data: UnsafeCell<ManuallyDrop<T>>,
 }
 
 pub struct Arc<T> {
@@ -20,7 +23,8 @@ impl<T> Arc<T> {
         Arc {
             ptr: NonNull::from(Box::leak(Box::new(ArcData {
                 ref_count: AtomicUsize::new(1),
-                data,
+                weak_count: AtomicUsize::new(1),
+                data: UnsafeCell::new(ManuallyDrop::new(data)),
             }))),
         }
     }
@@ -30,13 +34,60 @@ impl<T> Arc<T> {
         // (see new() and drop() impl). When dropping the last Arc, it also drops the ArcData.
         unsafe { self.ptr.as_ref() }
     }
+
+    pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
+        // Acquire matches Weak::drop's release decrement, to make sure any upgraded pointers are
+        // visible in the next ref_count.load.
+        if arc
+            .data()
+            .weak_count
+            .compare_exchange(1, usize::MAX, Acquire, Relaxed)
+            .is_err()
+        {
+            return None;
+        }
+        let is_unique = arc.data().ref_count.load(Relaxed) == 1;
+        // Release matches Acquire increment in 'downgrade', to make sure any changes to the
+        // ref_count that comes after 'downgrade' don't change the is_unique result above.
+        arc.data().weak_count.store(1, Release);
+        if !is_unique {
+            return None;
+        }
+        // Acquire to match Arc::drop's Release decrement, to make sure nothing else is accessing
+        // the data.
+        fence(Acquire);
+        unsafe { Some(&mut *arc.data().data.get()) }
+    }
+
+    pub fn downgrade(arc: &Self) -> Weak<T> {
+        let mut n = arc.data().weak_count.load(Relaxed);
+        loop {
+            if n == usize::MAX {
+                std::hint::spin_loop();
+                n = arc.data().weak_count.load(Relaxed);
+                continue;
+            }
+            assert!(n < usize::MAX - 1);
+            // Acquire synchronizes with get_mut's release-store.
+            if let Err(e) = arc
+                .data()
+                .weak_count
+                .compare_exchange_weak(n, n + 1, Acquire, Relaxed)
+            {
+                n = e;
+                continue;
+            }
+            return Weak { ptr: arc.ptr };
+        }
+    }
 }
 
 impl<T> Deref for Arc<T> {
     type Target = T;
 
     fn deref(&self) -> &T {
-        &self.data().data
+        // SAFETY: if there is an Active Arc, there is data
+        unsafe { &*self.data().data.get() }
     }
 }
 
@@ -54,12 +105,65 @@ impl<T> Drop for Arc<T> {
     fn drop(&mut self) {
         // TODO: add memory ordering.
         if self.data().ref_count.fetch_sub(1, Release) == 1 {
-            // NOTE: we can't use Relaxed ordering because we need to make sure nothing is still
+            // we can't use Relaxed ordering because we need to make sure nothing is still
             // accessing the dat awhen we drop it. every Clone must happen before a Drop so the
             // final fetch_sub would have to happen before all the others which we can do with the
             // Release Acquire ordering.
             fence(Acquire);
             // SAFETY: see comment in Arc::data()
+            unsafe {
+                ManuallyDrop::drop(&mut *self.data().data.get());
+            }
+            drop(Weak { ptr: self.ptr });
+        }
+    }
+}
+
+pub struct Weak<T> {
+    ptr: NonNull<ArcData<T>>,
+}
+
+unsafe impl<T: Send + Sync> Send for Weak<T> {}
+unsafe impl<T: Send + Sync> Sync for Weak<T> {}
+
+impl<T> Weak<T> {
+    fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
+    }
+
+    pub fn upgrade(&self) -> Option<Arc<T>> {
+        let mut n = self.data().ref_count.load(Relaxed);
+        loop {
+            if n == 0 {
+                return None;
+            }
+            assert!(n < usize::MAX);
+            if let Err(e) = self
+                .data()
+                .ref_count
+                .compare_exchange_weak(n, n + 1, Relaxed, Relaxed)
+            {
+                n = e;
+                continue;
+            }
+            return Some(Arc { ptr: self.ptr });
+        }
+    }
+}
+
+impl<T> Clone for Weak<T> {
+    fn clone(&self) -> Self {
+        if self.data().weak_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
+            std::process::abort();
+        }
+        Weak { ptr: self.ptr }
+    }
+}
+
+impl<T> Drop for Weak<T> {
+    fn drop(&mut self) {
+        if self.data().weak_count.fetch_sub(1, Release) == 1 {
+            fence(Acquire);
             unsafe {
                 drop(Box::from_raw(self.ptr.as_ptr()));
             }
--- tests/arc.rs
+++ tests/arc.rs
@@ -1,6 +1,8 @@
-use atomics_locks::arc::Arc;
+use atomics_locks::arc::{Arc, Weak};
+use atomics_locks::spinlock::SpinLock;
 use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering::Relaxed;
+use std::thread;
 
 #[test]
 fn arc() {
@@ -17,19 +19,141 @@ fn arc() {
     // Create two Arcs shares an object containing a string and a DetectDrop, to detect when it's
     // dropped.
     let x = Arc::new(("hello world", DetectDrop));
-    let y = x.clone();
+    let weak1 = Arc::downgrade(&x);
+    let weak2 = Arc::downgrade(&x);
 
     let t = std::thread::spawn(move || {
-        assert_eq!(x.0, "hello world");
+        // Weak pointer should be upgradeable
+        let y = weak1.upgrade().unwrap();
+        assert_eq!(y.0, "hello world");
     });
 
-    assert_eq!(y.0, "hello world");
+    assert_eq!(x.0, "hello world");
 
     t.join().unwrap();
 
+    // the data shouldn't be dropped yet
     assert_eq!(NUM_DROPS.load(Relaxed), 0);
+    assert!(weak2.upgrade().is_some());
 
-    drop(y);
+    drop(x);
 
+    // Now, the data should be dropped, and the weak pointer should bo longer be upgradeable.
     assert_eq!(NUM_DROPS.load(Relaxed), 1);
+    assert!(weak2.upgrade().is_none());
+}
+
+#[test]
+fn arc_spinlock() {
+    #[derive(Clone)]
+    enum Sex {
+        Male,
+        Female,
+    }
+    struct Person {
+        name: &'static str,
+        sex: Sex,
+        children: Vec<Arc<SpinLock<Person>>>,
+        mother: Option<Weak<SpinLock<Person>>>,
+        father: Option<Weak<SpinLock<Person>>>,
+    }
+
+    impl Person {
+        pub fn is_born(
+            mother: Option<&Arc<SpinLock<Self>>>,
+            father: Option<&Arc<SpinLock<Self>>>,
+            name: &'static str,
+            sex: Sex,
+        ) -> Result<Arc<SpinLock<Self>>, &'static str> {
+            match (mother, father) {
+                (Some(mother), Some(father)) => {
+                    let mut mother_guard = mother.lock();
+                    let mut father_guard = father.lock();
+                    match (&mother_guard.sex, &father_guard.sex) {
+                        (&Sex::Female, &Sex::Male) => {
+                            let baby = Arc::new(SpinLock::new(Person {
+                                name,
+                                children: Vec::new(),
+                                sex,
+                                mother: Some(Arc::downgrade(mother)),
+                                father: Some(Arc::downgrade(father)),
+                            }));
+                            {
+                                mother_guard.children.push(baby.clone());
+                                father_guard.children.push(baby.clone());
+                            }
+
+                            Ok(baby)
+                        }
+                        _ => Err("it takes a man and a woman to have a baby."),
+                    }
+                }
+                _ => Ok(Arc::new(SpinLock::new(Person {
+                    name,
+                    children: Vec::new(),
+                    sex,
+                    mother: None,
+                    father: None,
+                }))),
+            }
+        }
+    }
+
+    let madison = Person::is_born(None, None, "madison", Sex::Female).unwrap();
+    let mut carter = Person::is_born(None, None, "carter", Sex::Male).unwrap();
+
+    let babies = vec![
+        ("ayla", Sex::Female),
+        ("kane", Sex::Male),
+        ("kali", Sex::Male),
+        ("iris", Sex::Female),
+        ("nora", Sex::Female),
+    ];
+
+    thread::scope(|s| {
+        for baby in babies {
+            s.spawn(|| {
+                let baby_result = Person::is_born(Some(&madison), Some(&carter), baby.0, baby.1);
+                match baby_result {
+                    Ok(baby) => {
+                        assert!(
+                            Weak::upgrade(baby.lock().father.as_ref().unwrap())
+                                .unwrap()
+                                .lock()
+                                .name
+                                == "carter"
+                        );
+                        assert_eq!(
+                            Weak::upgrade(baby.lock().mother.as_ref().unwrap())
+                                .unwrap()
+                                .lock()
+                                .name,
+                            "madison"
+                        );
+                        println!("{} was born!", baby.lock().name);
+                    }
+                    Err(e) => println!("{}", e),
+                }
+            });
+        }
+    });
+
+    assert_eq!(carter.lock().children.len(), 5);
+
+    let cloned = carter.clone();
+
+    let carter_mut_option = Arc::get_mut(&mut carter);
+    if carter_mut_option.is_some() {
+        let baby_result = Person::is_born(Some(&madison), Some(&carter), "iris", Sex::Female);
+        match baby_result {
+            Ok(baby) => {
+                println!("{} was born!", baby.lock().name);
+            }
+            Err(e) => println!("{}", e),
+        };
+    }
+
+    assert!(carter.lock().children.len() == 5);
+
+    drop(cloned);
 }
